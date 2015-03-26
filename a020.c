88fa6bdf01af4edae3b0cf06d205ac644fd37a62
jd615645 <jd615645@gmail.com>
2015-03-26 21:39:55 +0800

    Update a020.c

--- a020.c
+++ a020.c
@@ -5,7 +5,7 @@ int main()
     char id[10];
     int tot,i,id_num[11];
     int chg[26]={10,11,12,13,14,15,16,17,34,18,19,20,21,22,35,23,24,25,26,27,28,29,32,30,31,33};
-    while(scanf("%s",&id) != EOF)
+    while(scanf("%s",id) != EOF)
     {
         tot=0;
         if(id[0]>='a' && id[0]<='z')
@@ -20,13 +20,13 @@ int main()
         {
             id_num[i]=id[i-1]-'0';
         }
-
+        tot=id_num[0];
         for(i=1;i<10;i++)
         {
             tot+=id_num[i]*(10-i);
         }
 
-        printf("%s\n",((10-(tot%10))%10==id_num[10])?"fake":"real");
+        printf("%s\n",((tot+id_num[10])%10!=0)?"fake":"real");
     }
 
     return 0;
