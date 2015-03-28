738ced0caab1e1b7243415853a5a22b47f1bf5d1
Jason <jd61645@gmail.com>
2015-03-28 14:21:03 +0800

    20150328

---
+++ AC/d705.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int p[46341]={2,3,5};
+int p_cnt=3;
+
+void build_prime();
+int is_prime(int a);
+
+int main()
+{
+	int in;
+	build_prime();
+	
+	while(scanf("%d",&in) == 1&&in)
+	{
+		printf("%c\n",is_prime(in) && in!=1?'0':'1');
+	}
+		
+	return 0;
+}
+
+void build_prime()
+{
+	int i,j;
+	
+	for(i=7,j=4; i<=46341 ; i+=j,j=6-j)
+	{
+		if(is_prime(i))
+		{
+			p[p_cnt]=i;
+			p_cnt++;
+		}
+	}
+}
+
+int is_prime(int a)
+{
+	int i,j;
+	for(i=0 ; i<p_cnt && p[i]*p[i]<=a ; i++)
+	{
+		if(a%p[i] == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
